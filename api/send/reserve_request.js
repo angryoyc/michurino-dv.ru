03d98a25f6ae268322bc7765f6aa1ecb54f72d2a
Serg A. Osipov <serg.osipov@gmail.com>
2016-10-03 15:15:31 +0000

    modified:   api/send/reserve_request.js 	modified:   sql/!!config.conf.example

--- api/send/reserve_request.js
+++ api/send/reserve_request.js
@@ -21,7 +21,7 @@ exports.do=function(arg, callback, callback_err, idata){
 	var text = 'Поступил запрос на бронирование участка #' + pp + '. Был указан телефон: ' + phone;
 	if(phone){
 		var emailconf = conf.email[conf.email.checked];
-		delivery.mail.send({to: 'serg.osipov@gmail.com', subj: 'Запос с сайта MICHURINO-DV.RU', text: text}, emailconf, 0, function(result){
+		delivery.mail.send({to: emailconf.user, subj: 'Запос с сайта MICHURINO-DV.RU', text: text}, emailconf, 0, function(result){
 			cf.mergeInto(data, result);
 			callback(data);
 		}, callback_err);
