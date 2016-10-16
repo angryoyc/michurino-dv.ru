41fbd0bd153a60506b7f781e2bcda77f3083f47c
Serg A. Osipov <serg.osipov@gmail.com>
2016-10-16 18:48:23 +1000

    modified:   controllers/root.js 	new file:   static/svg/decor_header_mirr.svg 	new file:   static/svg/main_decorated_head_mirr.svg 	modified:   views/root/main.jade

--- controllers/root.js
+++ controllers/root.js
@@ -4,7 +4,7 @@ module.exports={
 		res.render('root/index', {ng_app:'root', req:req, conf:conf});
 	},
 	main: function(req, res){
-		res.render('root/main', {ng_app:'root', req:req, conf:conf});
+		res.render('root/main', {ng_app:'root', req:req, conf:conf, fs: require('fs')});
 	},
 	schem: function(req, res){
 		res.render('root/schem', {ng_app:'root', req:req, conf:conf, fs: require('fs')});
