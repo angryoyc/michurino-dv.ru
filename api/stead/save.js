195b125516f09b86b176e12d59909ee76a4e9a05
Serg A. Osipov <serg.osipov@gmail.com>
2016-11-09 00:43:51 +1000

    modified:   api/stead/save.js

--- api/stead/save.js
+++ api/stead/save.js
@@ -21,47 +21,53 @@ exports.do=function(arg, callback, callback_err, idata){
 	var start = arg.start?arg.start:'';
 	var price = arg.price?parseInt(arg.price):0;
 	var cost = 550;
-	
-	console.log('PRICE', price)
 
 	if(idstead){
 		// проверим пользователя на существование
-		db.sql("select * from m.steads where idstead=$1;", [ idstead ], function(result){
+		db.sql("select * from m.params where param='cost';", [ ], function(result){
 			if(result.rows.length>0){
-				var stead = result.rows[0];
-				async.parallel(
-					[
-						function(cb){
-							db.sql("update m.steads set status=$2 where idstead=$1;", [ stead.idstead, status], function(result){
-								cb();
-							}, cb);
-						},
-						function(cb){
-							db.sql("update m.steads set points=$2 where idstead=$1;", [ stead.idstead, points], function(result){
-								cb();
-							}, cb);
-						},
-						function(cb){
-							db.sql("update m.steads set start=$2 where idstead=$1;", [ stead.idstead, start], function(result){
-								cb();
-							}, cb);
-						},
-						function(cb){
-							db.sql("update m.steads set price=$2 where idstead=$1 returning idstead, price;", [ stead.idstead, 1*price], function(result){
-								cb();
-							}, cb);
-						}
-					],
-					function(err){
-						if(err){callback_err(err)}else{
-							// и вернём данные пользователя
-							stead_data_ro(stead, callback, callback_err, data);
-						};
-					}
-				);
-			}else{
-				callback_err(env.error(104));
+				cost=parseInt(result.rows[0].val);
 			};
+			db.sql("select * from m.steads where idstead=$1;", [ idstead ], function(result){
+				if(result.rows.length>0){
+					var stead = result.rows[0];
+					async.parallel(
+						[
+							function(cb){
+								db.sql("update m.steads set status=$2 where idstead=$1;", [ stead.idstead, status], function(result){
+									cb();
+								}, cb);
+							},
+							function(cb){
+								db.sql("update m.steads set points=$2 where idstead=$1;", [ stead.idstead, points], function(result){
+									cb();
+								}, cb);
+							},
+							function(cb){
+								db.sql("update m.steads set start=$2 where idstead=$1;", [ stead.idstead, start], function(result){
+									cb();
+								}, cb);
+							},
+							function(cb){
+								db.sql("update m.steads set price=$2 where idstead=$1 returning idstead, price, s;", [ stead.idstead, 1*price], function(result){
+									var k = 1 * result.rows[0].price/(1 * result.rows[0].s * cost);
+									db.sql("update m.steads set k=$2 where idstead=$1 returning idstead, k;", [ stead.idstead, k], function(result){
+										cb();
+									}, cb);
+								}, cb);
+							}
+						],
+						function(err){
+							if(err){callback_err(err)}else{
+								// и вернём данные пользователя
+								stead_data_ro(stead, callback, callback_err, data);
+							};
+						}
+					);
+				}else{
+					callback_err(env.error(104));
+				};
+			}, callback_err);
 		}, callback_err);
 	}else{
 		callback_err(env.error(109));
