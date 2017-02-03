7623a85973c9b27e2990d49765d6b28e68fc711c
Serg A. Osipov <serg.osipov@gmail.com>
2017-02-03 22:44:20 +1000

    modified:   static/js/api.js 	renamed:    static/js/api.js -> static/js/api.js.old

--- static/js/api.js
+++ static/js/api.js
@@ -1,28 +1,38 @@
 angular.module('api', []);
-angular.module('api', []).factory('api', function($http, $q){
+angular.module('api', []).factory('api', function($http, $q, $timeout){
 	var self = {
 		call: function(url, data, showmessages, andurl){
-			var deferred = $q.defer();
-			
-			$http({method: 'POST', data: data, url: url}).
-			success(function(result, status, headers, config){
-				if(result.error==0){
-					deferred.resolve(result.data);
-				}else{
-					var mess = 'Ошибка: ' + (andurl?('['+url+'] - '):'') + result.message;
-					if(showmessages) swal(mess);
-					deferred.reject(result);
-				};
-			}).
-			error(function(result, status, headers, config) {
-				var mess = 'Ошибка http (см. консоль.)';
-				if(showmessages) swal(mess);
-				console.log(result);
-				deferred.reject({error: 500, message:mess});
-			});
-
-			return deferred.promise;
-
+			return $http({
+				method: 'POST',
+				url: url,
+				data: data
+			})
+			.then(
+				function(response){
+					if(response.data.error==0){
+						return response.data.data;
+					}else{
+						var mess = 'Ошибка: ' + (andurl?('['+url+'] - '):'') + response.data.message;
+						var err = new Error(mess);
+						err.error=response.data.error;
+						if(showmessages) {
+							self.error(new Error(mess));
+						};
+						throw err;
+					};
+				},
+				function(err){
+					if(showmessages) {
+						self.error({message:err.statusText});
+					};
+					throw err;
+				}
+			)
+		},
+		error: function(err){
+			$timeout(function(){
+				swal('ОШИБКА!', err.message || err.statusText, 'error');
+			}, 100);
 		}
 	};
 	return self;
